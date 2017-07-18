7b492ababbdde9ed6a4a55edc61edd493081125e
hjfeng1988 <hjfeng1988@163.com>
2017-07-18 16:15:29 +0800

    update

--- salt/prod/jdk/config/jdk.sh
+++ salt/prod/jdk/config/jdk.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=usrlocaljdk1.7.0_80
-export CLASSPATH=.$JAVA_HOMElibdt.jar$JAVA_HOMElibtools.jar
-export PATH=$PATH$JAVA_HOMEbin
+export JAVA_HOME=/usr/local/jdk1.7.0_80
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export PATH=$PATH:$JAVA_HOME/bin
